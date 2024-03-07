4c9b10f66fb1a81656dc7d5139885a7a09f3b187
xjfkkk <155220096+xjfkkk@users.noreply.github.com>
2024-03-07 18:55:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  *  自定义网站配置 
  */
 const config = {
-  title: "自定义导航",                 //write your website title
+  title: "自定义导航 ",                 //write your website title
   subtitle: "Cloudflare Workers Dir", //write your website subtitle
   logo_icon: "sitemap",               //select your logo by semantic-ui icon (you can get more msg in:https://semantic-ui.com/elements/icon.html)
   hitokoto: true,                     //use hitokoto or not
